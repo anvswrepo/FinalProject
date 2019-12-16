6c0c986b30e49fbb01b58172a3453d0ad7c83436
Suneetha Malkani <suneetha@radar108.com>
2019-12-16 13:17:48 -0500

    updated show api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,35 +24,84 @@ class UsersController < ApplicationController
   # end
 
   
-  # user and cart and cart items and product name
-  def show
-    user_carts = @user.usercarts
-    user_cartsid = @user.usercarts.first 
+  # user and cart and cart items and product name -- V2
+  # def show
+  #   user_carts = @user.usercarts
+  #   user_cartsid = @user.usercarts.first 
     
-    user_items = user_cartsid.cartitems
+  #   user_items = user_cartsid.cartitems
+    
+  #   productslist2 = []
+  #   user_items.each { |id| 
+  #   puts " id #{id}"
+  # puts id.product_id
+  #  prod_id =  id.product_id
+  # puts Product.find(prod_id)
+  # puts Product.find(prod_id).name
+  # puts Product.find(prod_id).category
+  #   singleprod = { item: id,  name:  Product.find(prod_id).name, category:  Product.find(prod_id).category,
+  # price:  Product.find(prod_id).price, quantity:  Product.find(prod_id).quantity_instock
+  # }
+  # productslist2.push(singleprod)
+  # }
+
+  # puts productslist2
+  #   render json: { user: @user, carts: user_carts, items:  user_items, products: productslist2  } 
+  # end
+
+
+
+  # user and cart and cart items and product name -- V3
+    def show
+      user_carts = @user.usercarts
+      user_cartsid = @user.usercarts.first 
+      
+      user_items = user_cartsid.cartitems
+
+      productslist2 = []
+    
+      user_items.each { |id| 
+      puts " id #{id}"
+    puts id.product_id
+     prod_id =  id.product_id
+    puts Product.find(prod_id)
+    puts Product.find(prod_id).name
+    puts Product.find(prod_id).category
+      
+    singleprod = { id: id.id, user_quantity: id.quantity, product_id: id.product_id,  name:  Product.find(prod_id).name, category:  Product.find(prod_id).category,
+    price:  Product.find(prod_id).price, quantity:  Product.find(prod_id).quantity_instock
+    }
+
+    # singleprod = { item: id,  name:  Product.find(prod_id).name, category:  Product.find(prod_id).category,
+    # price:  Product.find(prod_id).price, quantity:  Product.find(prod_id).quantity_instock
+    # }
+    productslist2.push(singleprod)
+    }
+
+
+
+      user_details = {
+        "id": @user.id,
+        "username": @user.username,
+        "email": @user.email,
+        "cart_id": user_cartsid.id,
+       "last_accessed_date": user_cartsid.last_accessed_date,
+       "cartitems": productslist2
+      }
+
+
+  
+    puts productslist2
+
+    # render json: { user: @user, carts: user_carts, items:  user_items, products: productslist2  } 
+      render json: { user: user_details  } 
+    end
+  
+
+
     # user_item_products = user_cartsid.cartitems.products
-    
-    productslist2 = []
-    user_items.each { |id| 
-    puts " id #{id}"
-  puts id.product_id
-   prod_id =  id.product_id
-  puts Product.find(prod_id)
-  puts Product.find(prod_id).name
-  puts Product.find(prod_id).category
-    singleprod = { item: id,  name:  Product.find(prod_id).name, category:  Product.find(prod_id).category,
-  price:  Product.find(prod_id).price, quantity:  Product.find(prod_id).quantity_instock
-  }
-  productslist2.push(singleprod)
-  }
-
-  puts productslist2
-    # user_itemsid = user_cartsid.cartitems.first
+      # user_itemsid = user_cartsid.cartitems.first
     # user_product = user_cartsid.cartitems
-    
-    render json: { user: @user, carts: user_carts, items:  user_items, products: productslist2  } 
-  end
-
 
 
   # POST /users
